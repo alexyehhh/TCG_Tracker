c190201cc8c724419858ade7da82f1cb40b2f022
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-10-15 21:50:42 -0700

    able to get the rectagular looking thing, still need to fix fonts, and icons

--- client/src/routes/HomePage/HomePage.jsx
+++ client/src/routes/HomePage/HomePage.jsx
@@ -2,37 +2,38 @@ import React from 'react';
 import magnifyingGlass from '../../assets/images/magnifyingGlass.png';
 import charizard from '../../assets/images/charizard.png';
 import './HomePage.css';
+
 export default function HomePage() {
 	return (
-		<div>
-			{/* Nav Bar */}
-			<header>
-				<nav className='navbar'>
-					<div className='nav-title' />
-					<ul className='nav-links'>
-						<li>
-							<a href='#'>Search</a>
-						</li>
-						<li>
-							<a href='#'>Collection</a>
-						</li>
-						<li>
-							<a href='#'>Upload</a>
-						</li>
-					</ul>
-					<a href='#' className='sign-in-btn'>
-						Sign in &gt;
-					</a>
-				</nav>
-			</header>
-			{/* Main Content */}
-			<main>
-				<div className='container'>
-					{/* Left Side: search bar and the description */}
+		<div className='homepage'>
+			<div className='background-clip'></div>
+			<div className='content-wrapper'>
+				<header>
+					<nav className='navbar'>
+						<div className='navbar-left'></div>
+						<ul className='nav-links'>
+							<li>
+								<a href='#'>Search</a>
+							</li>
+							<li>
+								<a href='#'>Collection</a>
+							</li>
+							<li>
+								<a href='#'>Upload</a>
+							</li>
+						</ul>
+						<div className='navbar-right'>
+							<a href='#' className='sign-in-btn'>
+								Sign in &gt;
+							</a>
+						</div>
+					</nav>
+				</header>
+
+				<main className='content-container'>
 					<div className='left-content'>
 						<h1>
-							Find your <br />
-							Pokémon Collection’s Worth
+							Find your <br /> Pokémon <br /> Collection's Worth
 						</h1>
 						<p>
 							This will change the way you track the prices of your Pokemon
@@ -45,12 +46,12 @@ export default function HomePage() {
 							</button>
 						</div>
 					</div>
-					{/* Right Side: Charizard card (maybe randomize different card img?)*/}
+
 					<div className='right-content'>
 						<img src={charizard} alt='Charizard Card' className='card-image' />
 					</div>
-				</div>
-			</main>
+				</main>
+			</div>
 		</div>
 	);
 }
