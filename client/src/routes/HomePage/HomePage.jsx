6caf6f2248fb61375fb34f615e03cd4877e0586a
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-10-17 19:08:51 -0700

    using css submodules for homepage.jsx

--- client/src/routes/HomePage/HomePage.jsx
+++ client/src/routes/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import magnifyingGlass from '../../assets/images/magnifyingGlass.png';
 import charizard from '../../assets/images/charizard.png';
-import './HomePage.css';
+import styles from './HomePage.module.css';
 
 export default function HomePage() {
 	const cardRef = useRef(null);
@@ -30,13 +30,13 @@ export default function HomePage() {
 		};
 	}, []);
 	return (
-		<div className='homepage'>
-			<div className='background-clip'></div>
-			<div className='content-wrapper'>
+		<div className={styles.homepage}>
+			<div className={styles['background-clip']}></div>
+			<div className={styles['content-wrapper']}>
 				<header>
-					<nav className='navbar'>
-						<div className='navbar-left'></div>
-						<ul className='nav-links'>
+					<nav className={styles.navbar}>
+						<div className={styles['navbar-left']}></div>
+						<ul className={styles['nav-links']}>
 							<li>
 								<a href='#'>Search</a>
 							</li>
@@ -47,24 +47,24 @@ export default function HomePage() {
 								<a href='#'>Upload</a>
 							</li>
 						</ul>
-						<div className='navbar-right'>
-							<a href='/signin' className='sign-in-btn'>
+						<div className={styles['navbar-right']}>
+							<a href='/signin' className={styles['sign-in-btn']}>
 								Sign in &gt;
 							</a>
 						</div>
 					</nav>
 				</header>
 
-				<main className='content-container'>
-					<div className='left-content'>
-						<h1 className='left-content-title'>
+				<main className={styles['content-container']}>
+					<div className={styles['left-content']}>
+						<h1 className={styles['left-content-title']}>
 							Find your <br /> Pokémon <br /> Collection's Worth
 						</h1>
-						<p className='left-content-subtitle'>
+						<p className={styles['left-content-subtitle']}>
 							This will change the way you track the prices of your Pokemon
 							cards. Search your card below.
 						</p>
-						<div className='search-bar'>
+						<div className={styles['search-bar']}>
 							<input type='text' placeholder='Search for your card...' />
 							<button>
 								<img src={magnifyingGlass} alt='Search' width='15px' />
@@ -72,8 +72,8 @@ export default function HomePage() {
 						</div>
 					</div>
 
-					<div className='right-content'>
-						<div className='card' ref={cardRef}>
+					<div className={styles['right-content']}>
+						<div className={styles.card} ref={cardRef}>
 							<img
 								src={charizard}
 								alt='Charizard Card'
