069e9291581672d65cebeb1e90bd0ceab0db69c1
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-10-25 16:02:22 -0700

    changed SignIn to LogIn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './routes/HomePage/HomePage';
 import SignUp from './routes/SignUp/SignUp'; // http://localhost:5173/signup
-import SignIn from './routes/SignIn/SignIn'; // http://localhost:5173/signin
+import LogIn from './routes/LogIn/LogIn'; // http://localhost:5173/login
 import BasicCard from './routes/BasicCard/BasicCard'; // http://localhost:5173/basic-card
 import CardDetail from './routes/CardDetail/CardDetail'; // ex for card detail: http://localhost:5173/card-detail/base1-2
 import PokemonCards from './routes/PokemonCards/PokemonCards'; // http://localhost:5173/pokemon-cards -> sends to pikachu for default
@@ -15,7 +15,7 @@ function App() {
 			<div className='App'>
 				<Routes>
 					<Route path='/' element={<HomePage />} />
-					<Route path='/login' element={<SignIn />} />
+					<Route path='/login' element={<LogIn />} />
 					<Route path='/signup' element={<SignUp />} />
 					<Route path='/forgot-password' element={<ForgotPassword />} />
 					<Route path='/basic-card' element={<BasicCard />} />
