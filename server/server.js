957acc633c858c3697412b68cf53f7500a53f785
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-11-02 23:05:15 -0700

    moved ebay auth to its own file and getCardPrices to route file

--- server/server.js
+++ server/server.js
@@ -1,15 +1,13 @@
-import express from 'express';
-import cors from 'cors';
-import cardPricesRoute from './getCardPrices.mjs';
-import dotenv from 'dotenv';
-
-dotenv.config();
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const cardPricesRoute = require('./routes/getCardPrices.js');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(cardPricesRoute);
+app.use('/', cardPricesRoute);
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
