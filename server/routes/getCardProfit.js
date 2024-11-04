db3d30c84f85c983b001f8c71f0f231f343b5c50
Evan L <>
2024-11-03 19:48:48 -0800

    gamestop profit calculations

---
+++ server/routes/getCardProfit.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+// const fetch = require('node-fetch');
+// const { getPokemonToken } = require('../util/pkmnAuth.js');
+
+router.get('/card-profit', async (req, res) => {
+	console.log('Received profit request:', req.query);
+    const salePrice = req.query.salePrice;
+    const pricePaid = req.query.pricePaid;
+
+	if (!pricePaid) {
+		return res.status(400).json({ error: 'Price Paid is required' });
+	}
+
+	try {
+        const gmeAdditionalFees = 15.99 + (isMember ? 0 : 4.99);
+        const gmeProfit =  salePrice - (pricePaid + gmeAdditionalFees);
+
+		res.json({
+			gmeProfit: gmeProfit,
+		});
+	} catch (error) {
+		console.error('Error fetching card profit:', error);
+		res.status(500).json({
+			error: 'Failed to fetch card profit',
+			details: error.message,
+		});
+	}
+});
+
+module.exports = router;
