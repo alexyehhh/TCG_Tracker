4f90724a98b22d257f145e52f5ca26bf9934464d
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-11-04 11:15:50 -0800

    added cardProfit route to server.js and removed cardsearch

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const cardPricesRoute = require('./routes/getCardPrices.js');
+const cardProfit = require('./routes/getCardProfit.js');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use('/', cardPricesRoute);
+app.use('/', cardProfit);
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
