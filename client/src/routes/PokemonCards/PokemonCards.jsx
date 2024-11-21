4792efcf5bac1a3b06244978501b7b4d1d2fc4d4
Diane Poeng <95322564+dianee0@users.noreply.github.com>
2024-11-21 11:53:30 -0800

    finished adding pages for cards. Also optimized page loading, so that only the cards on that page are loaded. Added a spinner instead of 'loading cards...' text

--- client/src/routes/PokemonCards/PokemonCards.jsx
+++ client/src/routes/PokemonCards/PokemonCards.jsx
@@ -8,14 +8,13 @@ function PokemonCards() {
 	const [cards, setCards] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
+	const [totalPages, setTotalPages] = useState(1);
 	const location = useLocation();
+	const [currentPage, setCurrentPage] = useState(1);
+	const cardsPerPage = 30;
 
 	const query = new URLSearchParams(location.search);
 	const searchQuery = query.get('name') || 'Pikachu';
-
-	const handleBackToSearch = () => {
-		navigate('/');
-	};
 
 	const parseSearchQuery = (query) => {
 		const parts = query.trim().toLowerCase().split(' ');
@@ -39,51 +38,61 @@ function PokemonCards() {
 	const { name: pokemonName, number: cardNumber } =
 		parseSearchQuery(searchQuery);
 
-	useEffect(() => {
-		const fetchCards = async () => {
-			try {
-				let query;
+	const fetchCards = async (page) => {
+		try {
+			setLoading(true);
+			const offset = (page - 1) * cardsPerPage; // calculates the offset
+			let query;
 
-				// check if the pokemon name ends with EX upper case or not
-				if (pokemonName.toLowerCase().endsWith(' ex')) {
-					// query that matches both EX and -EX suffixes
-					const baseName = pokemonName.slice(0, -3).trim(); // remove EX from the end of the name
-					query = `(name:"${baseName} EX" OR name:"${baseName}-EX")`;
-				} else {
-					// default query for other search terms
-					query = `name:"${pokemonName}"`;
-				}
-
-				if (cardNumber) {
-					if (cardNumber.includes('TG')) {
-						query += ` (number:"${cardNumber}" OR number:"${
-							cardNumber.split('/')[0]
-						}")`;
-					} else {
-						query += ` number:"${cardNumber}"`;
-					}
-				}
-
-				const response = await axios.get(
-					`https://api.pokemontcg.io/v2/cards?q=${query}`,
-					{
-						headers: { 'X-Api-Key': import.meta.env.VITE_POKEMON_KEY },
-					}
-				);
-				setCards(response.data.data);
-			} catch (err) {
-				setError(`Failed to fetch cards. Error: ${err}`);
-			} finally {
-				setLoading(false);
+			// check if the pokemon name ends with EX upper case or not
+			if (pokemonName.toLowerCase().endsWith(' ex')) {
+				// query that matches both EX and -EX suffixes
+				const baseName = pokemonName.slice(0, -3).trim(); // remove EX from the end of the name
+				query = `(name:"${baseName} EX" OR name:"${baseName}-EX")`;
+			} else {
+				// default query for other search terms
+				query = `name:"${pokemonName}"`;
 			}
-		};
 
-		fetchCards();
-	}, [pokemonName, cardNumber]);
+			if (cardNumber) {
+				if (cardNumber.includes('TG')) {
+					query += ` (number:"${cardNumber}" OR number:"${
+						cardNumber.split('/')[0]
+					}")`;
+				} else {
+					query += ` number:"${cardNumber}"`;
+				}
+			}
+
+			const response = await axios.get(
+				`https://api.pokemontcg.io/v2/cards?q=${query}&pageSize=${cardsPerPage}&page=${page}`,
+				{
+					headers: { 'X-Api-Key': import.meta.env.VITE_POKEMON_KEY },
+				}
+			);
+			setCards(response.data.data);
+			setTotalPages(Math.ceil(response.data.totalCount / cardsPerPage)); // Dynamically calculate total pages
+		} catch (err) {
+			setError(`Failed to fetch cards. Error: ${err}`);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	// fetch only when pokemonName, cardNumber, currentPage are changed
+	useEffect(() => {
+		fetchCards(currentPage);
+	}, [pokemonName, cardNumber, currentPage]);
+
+	const paginate = async (pageNumber) => {
+		setError(null); // Reset error
+		setCurrentPage(pageNumber);
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	};
 
 	const renderContent = () => {
 		if (loading) {
-			return <h1 className={styles.centerContent}>Loading cards...</h1>;
+			return <div className={styles.centerContent}><div className={styles.spinner}></div></div>;
 		}
 
 		if (error) {
@@ -147,6 +156,30 @@ function PokemonCards() {
 			<div className={styles.pokemonCards}>
 				{cards.length > 0 && <h1>Cards for {searchQuery}</h1>}
 				{renderContent()}
+				<div className={styles.pagination}>
+					{!loading && (
+						<>
+							<button
+								onClick={() => paginate(currentPage - 1)}
+								disabled={currentPage === 1}>
+								Previous
+							</button>
+							{Array.from({ length: totalPages }, (_, i) => (
+								<button
+									key={i + 1}
+									onClick={() => paginate(i + 1)}
+									className={currentPage === i + 1 ? styles.activePage : ''}>
+									{i + 1}
+								</button>
+							))}
+							<button
+								onClick={() => paginate(currentPage + 1)}
+								disabled={currentPage === totalPages}>
+								Next
+							</button>
+						</>
+					)}
+				</div>
 			</div>
 		</div>
 	);
