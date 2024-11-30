0c1fcf5d51d30e342f7635adbd8af4c706959c54
Diane Poeng <95322564+dianee0@users.noreply.github.com>
2024-11-30 13:03:06 -0800

    installed chart.js, graph seems to be working, still need to run tests for collection value over different days

---
+++ client/src/components/Graphs/GraphComponent.jsx
@@ -0,0 +1,63 @@
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Shapes } from 'lucide-react';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+const PriceHistoryGraph = ({ data }) => {
+    const chartData = {
+        labels: data.map((entry) => entry.date),
+        datasets: [
+            {
+                label: 'Total Price Over Time',
+                data: data.map((entry) => entry.total),
+                borderColor: 'rgba(94.0, 166.0, 80.0, 1)',
+                backgroundColor: 'rgba(94, 166, 80, 0.2)',
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false, // enables customization for graph size
+        plugins: {
+            legend: {
+                position: 'top',
+                labels: {
+                    color: '#581c87',
+                },
+                onClick: null, // disables default legend click functionality
+            },
+            title: {
+                display: true,
+                text: 'Collection Value Over Time',
+                color: '#581c87',
+                font: {
+                    size: 25,
+                    family: 'Arial',
+                    weight: 'bold',
+                },
+            },
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: '#581c87', // x axis text color
+                },
+            },
+            y: {
+                ticks: {
+                    color: '#581c87', // y axis text color
+                },
+            },
+        },
+    };
+
+    return (
+        <div style={{ width: '800px', height: '500px' }}>
+            <Line data={chartData} options={options} />
+        </div>
+    );
+};
+
+export default PriceHistoryGraph;
