d1d74174be92262003b293d3bfa14dbc62129b67
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-12-03 22:11:25 -0800

    removed firebaseRoutes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const cardPricesRoute = require('./routes/getCardPrices.js');
 const cardProfit = require('./routes/getCardProfit.js');
 const recognizeCardRoute = require('./routes/recognizeCard.js');
-// const firebaseRoutes = require('./routes/firebaseRoutes.js');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -13,7 +12,6 @@ app.use(cors());
 app.use('/', cardPricesRoute);
 app.use('/', cardProfit);
 app.use('/', recognizeCardRoute);
-// app.use('/', firebaseRoutes);
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
